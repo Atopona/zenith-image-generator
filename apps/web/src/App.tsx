9a93a9874d70724ea2272f88041a6d7c98c0a09d
wumingdao <wumingdao070@gmail.com>
2025-12-14 01:07:14 +0800

    remove other flow

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Toaster } from '@/components/ui/sonner'
-import ConversationFlowPage from './pages/ConversationFlowPage'
-import FlowPage from './pages/FlowPage'
+// import ConversationFlowPage from './pages/ConversationFlowPage'
+// import FlowPage from './pages/FlowPage'
 import FlowPageV2 from './pages/FlowPageV2'
 import ImageGenerator from './pages/ImageGenerator'
 
@@ -11,9 +11,9 @@ function App() {
       <Toaster />
       <Routes>
         <Route path="/" element={<ImageGenerator />} />
-        <Route path="/flow" element={<FlowPage />} />
-        <Route path="/flow2" element={<FlowPageV2 />} />
-        <Route path="/chat" element={<ConversationFlowPage />} />
+        <Route path="/flow" element={<FlowPageV2 />} />
+        {/* <Route path="/flow2" element={<FlowPageV2 />} /> */}
+        {/* <Route path="/chat" element={<ConversationFlowPage />} /> */}
       </Routes>
     </BrowserRouter>
   )
